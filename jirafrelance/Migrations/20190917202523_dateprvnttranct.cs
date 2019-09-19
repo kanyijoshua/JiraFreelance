835280b51ada50b228b6259525f1520df0d93a52
Joshua Kanyi <kanyiwest.jo@gmail.com>
2019-09-19 16:05:25 +0300

    enabled file download

---
+++ jirafrelance/Migrations/20190917202523_dateprvnttranct.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jirafrelance.Migrations
+{
+    public partial class dateprvnttranct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "bid_award_time",
+                table: "tbl_bid",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "bid_award_time",
+                table: "tbl_bid",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
---
+++ jirafrelance/Migrations/20190918205900_wksdate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jirafrelance.Migrations
+{
+    public partial class wksdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "wkspc_start_time",
+                table: "tbl_workspace",
+                newName: "wkspc_starttime");
+
+            migrationBuilder.RenameColumn(
+                name: "wkspc_expectend_end_time",
+                table: "tbl_workspace",
+                newName: "wkspc_expectend_endtime");
+
+            migrationBuilder.RenameColumn(
+                name: "wkspc_actual_end_time",
+                table: "tbl_workspace",
+                newName: "wkspc_actual_endtime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "wkspc_starttime",
+                table: "tbl_workspace",
+                newName: "wkspc_start_time");
+
+            migrationBuilder.RenameColumn(
+                name: "wkspc_expectend_endtime",
+                table: "tbl_workspace",
+                newName: "wkspc_expectend_end_time");
+
+            migrationBuilder.RenameColumn(
+                name: "wkspc_actual_endtime",
+                table: "tbl_workspace",
+                newName: "wkspc_actual_end_time");
+        }
+    }
+}
